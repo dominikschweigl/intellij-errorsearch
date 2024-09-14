b68327f87b9458127fec35a75877bbfc7d82e071
Dominik Schweigl <domnk.dev@gmail.com>
2024-09-14 10:35:24 +0200

    remove unnecessary imports

--- src/main/java/com/error_search/SearchErrorAction.java
+++ src/main/java/com/error_search/SearchErrorAction.java
@@ -1,23 +1,12 @@
 package com.error_search;
 
-import com.intellij.analysis.problemsView.Problem;
-import com.intellij.analysis.problemsView.ProblemsCollector;
 import com.intellij.analysis.problemsView.toolWindow.ProblemNode;
-import com.intellij.analysis.problemsView.toolWindow.ProblemsViewPanel;
 import com.intellij.ide.BrowserUtil;
-import com.intellij.ide.ProjectGroupActionGroup;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.FileViewProvider;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
-
-import java.util.Collection;
-import java.util.Objects;
 
 import static com.intellij.openapi.actionSystem.PlatformCoreDataKeys.SELECTED_ITEM;
 
@@ -28,7 +17,6 @@ public class SearchErrorAction extends AnAction {
         ProblemNode node = (ProblemNode) event.getData(SELECTED_ITEM);
         if (project == null || node == null) return;
 
-        PsiFile file = event.getData(CommonDataKeys.PSI_FILE);
         FileViewProvider provider = PsiManager.getInstance(project).findViewProvider(node.getVirtualFile());
         if (provider == null) return;
 
